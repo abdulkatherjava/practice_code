0505ce613999e6947071bc8a1dabf2c35a11a434
Abdul Kather <abdulkather2025@gmail.com>
2023-11-18 15:15:06 +0530

    java interview questions

---
+++ src/leetcode/Anagram.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String s1 = "ton";
+        String s2 = "noT";
+
+        Map<Character, Long> map1 = s1.chars()
+                .mapToObj(ch -> (char) Character.toUpperCase(ch))
+                .collect(Collectors.groupingBy(ch -> ch, Collectors.counting()));
+
+        Map<Character, Long> map2 = s2.chars()
+                .mapToObj(ch -> (char) Character.toUpperCase(ch))
+                .collect(Collectors.groupingBy(ch -> ch, Collectors.counting()));
+        if(map1.equals(map2)) {
+            System.out.println("It's Anagram");
+        } else {
+            System.out.println("it's not Anagram");
+        }
+    }
+}
